61745f912875514af4b08dff03020c5f9828238b
realDragon11 <43188351+realDragon11@users.noreply.github.com>
2019-10-02 06:16:20 -0400

    attempt to fix the examine modifier glitch

--- Dragon's Trawel/src/Attack.java
+++ Dragon's Trawel/src/Attack.java
@@ -233,7 +233,7 @@ public class Attack implements java.io.Serializable{
 	public Attack impair(int handLevel, TargetFactory.TargetType targetType) {
 		Target t = TargetFactory.randTarget(targetType);
 		Style s = StyleFactory.randStyle();
-		if (name != "examine") {
+		if (!name.contains("examine")) {
 		return new Attack(s.name + name + " " + t.name, hitMod*t.hit*s.hit,  (s.speed*speed)+extra.randRange(0,20)-10,  handLevel*s.damage*t.sharp*sharp*extra.hrandom(),  handLevel*s.damage*t.blunt*blunt*extra.hrandom(),  handLevel*s.damage*t.pierce*pierce*extra.hrandom(),  desc,soundStrength,soundType,t);
 		}else {
 			return this;
