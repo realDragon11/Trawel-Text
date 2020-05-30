7d46d4d2ca2f32b502eeb7aeae2bfd136db37bd2
realDragon11 <43188351+realDragon11@users.noreply.github.com>
2020-05-30 16:40:27 -0400

    probably should of hashmaped converted it

--- Dragon's Trawel/src/rtrawel/EventFlag.java
+++ Dragon's Trawel/src/rtrawel/EventFlag.java
@@ -13,4 +13,38 @@ public class EventFlag implements java.io.Serializable {
 		eventFlag = new EventFlag();
 	}
 	
+	
+	public int getEF(String str) {
+		for (int i = 0;i <events.size();i++) {
+			if (events.get(i).equals(str)) {
+				return flags.get(i);
+			}
+		}
+		return 0;
+	}
+	
+	public void addEF(String str, int a) {
+		for (int i = 0;i <events.size();i++) {
+			if (events.get(i).equals(str)) {
+				flags.add(i,flags.get(i)+a);
+				flags.remove(i+1);
+				return;
+			}
+		}
+		events.add(str);
+		flags.add(a);
+	}
+	
+	public void setEF(String str, int a) {
+		for (int i = 0;i <events.size();i++) {
+			if (events.get(i).equals(str)) {
+				flags.add(i,a);
+				flags.remove(i+1);
+				return;
+			}
+		}
+		events.add(str);
+		flags.add(a);
+	}
+	
 }
