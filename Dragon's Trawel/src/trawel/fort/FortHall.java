fe0bba2db921dca424a89689043f6e2747ce2dab
realDragon11 <43188351+realDragon11@users.noreply.github.com>
2020-11-05 11:25:41 -0500

    fort work

--- Dragon's Trawel/src/trawel/fort/FortHall.java
+++ Dragon's Trawel/src/trawel/fort/FortHall.java
@@ -1,10 +1,12 @@
 package trawel.fort;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import trawel.Combat;
 import trawel.Feature;
 import trawel.Person;
+import trawel.Player;
 import trawel.Skill;
 import trawel.Town;
 import trawel.mainGame;
@@ -18,6 +20,7 @@ import trawel.mainGame;
 public class FortHall extends FortFeature {
 
 	public int level;
+	public ArrayList<Person> allies = new ArrayList<Person>();
 	public FortHall(int tier, Town town) {
 		this.town = town;
 		this.level = tier;
@@ -46,6 +49,7 @@ public class FortHall extends FortFeature {
 
 	@Override
 	public void passTime(double time) {
+		
 	}
 	
 	public int getSkillCount(SubSkill s) {
@@ -59,22 +63,31 @@ public class FortHall extends FortFeature {
 	
 	public int getWatchScore() {
 		int i = 0;
-		
+		i+=getSkillCount(SubSkill.SCRYING)*3;
 		return i;
 	}
 	
 	public void Fight(ArrayList<Person>... people) {
 		Combat c = new Combat(this.town.getIsland().getWorld(),this,people);
+		allies.clear();
 		if (c.survivors.get(0).hasSkill(Skill.PLAYERSIDE)) {
-			
+			c.survivors.remove(Player.player.getPerson());
+			allies.addAll(c.survivors);
 		}else {
 			
 		}
 	}
 
-	private ArrayList<Person>[] getAllies() {
-		// TODO Auto-generated method stub
-		return null;
+	private ArrayList<Person> getAllies() {
+		while (allies.size() < 10) {
+			allies.add(new Person(level));
+		}
+		for (Person a: allies) {
+			if (!a.hasSkill(Skill.PLAYERSIDE)) {
+				a.addSkill(Skill.PLAYERSIDE);
+			}
+		}
+		return allies;
 	}
 
 }
