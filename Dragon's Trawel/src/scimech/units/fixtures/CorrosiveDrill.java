074a56edbafecdce1c54a9a201e210f1f8047915
realDragon11 <43188351+realDragon11@users.noreply.github.com>
2021-04-28 01:30:12 -0400

    hazmat

--- Dragon's Trawel/src/scimech/units/fixtures/CorrosiveDrill.java
+++ Dragon's Trawel/src/scimech/units/fixtures/CorrosiveDrill.java
@@ -10,24 +10,29 @@ import scimech.mech.Fixture;
 import scimech.mech.TurnSubscriber;
 import trawel.extra;
 
-public class AcidFoam extends Fixture {
+public class CorrosiveDrill extends Fixture {
 
 	@Override
 	public void activate(Target t, TurnSubscriber ts) {
 		int acc = (int) (10*rating());
 		int hits = 0;
-		for (int i = 0; i < 20;i++) {
-			double hit = MechCombat.computeHit(t, AimType.ARCING, acc);
+		for (int i = 0; i < 5;i++) {
+			double hit = MechCombat.computeHit(t, AimType.MELEE, acc);
 			if (hit  >=0) {
 				hits++;
+				t.takeDamage().take(DamageTypes.KINETIC,DamageMods.AP,4, t);
+				acc += 4;
 			}
+			if (hit < -5) {
+				break;
 			}
-		t.takeDamage().take(DamageTypes.CAUSTIC,DamageMods.HOLLOW,(int) (hits/1.1f), t);
-		t.takeDamage().suffer(DamageEffect.SLOW,hits*1.5, t);
-		t.takeDamage().suffer(DamageEffect.ACID,hits*5, t);
+		}
+		if (hits > 0) {
+			t.takeDamage().take(DamageTypes.CAUSTIC,DamageMods.HOLLOW,4, t);
+		}
 		if (!t.isDummy()) {
-			//currentMount.takeHeat(5);
-			extra.print("The Acid Foamer attacks! " + (hits*5) + "% effective! ");
+			currentMount.takeHeat(2);
+			extra.print("The Corrosive Drill attacks! " + hits + " hits! ");
 		}
 	}
 
@@ -38,37 +43,37 @@ public class AcidFoam extends Fixture {
 
 	@Override
 	public int heatCap() {
-		return 9;
+		return 15;
 	}
 
 	@Override
 	public String getName() {
-		return "Acid Foamer";
+		return "Corrosive Drill";
 	}
 
 	@Override
 	public int getEnergyDraw() {
-		return 5;
+		return 8;
 	}
 
 	@Override
 	public String getDescription() {
-		return "Deals some hollow caustic damage as well as slowing the enemy and bonus system damage.";
+		return "Deals some hollow caustic damage if it hits as repeated AP kinetic. Chain hits.";
 	}
 
 	@Override
 	public int getComplexity() {
-		return 10;
+		return 12;
 	}
 
 	@Override
 	public int getWeight() {
-		return 4;
+		return 10;
 	}
 
 	@Override
 	public int getSlots() {
-		return 4;
+		return 6;
 	}
 
 }
--- Dragon's Trawel/src/scimech/units/mechs/Hazmat.java
+++ Dragon's Trawel/src/scimech/units/mechs/Hazmat.java
@@ -7,7 +7,9 @@ import scimech.mech.Mech;
 import scimech.mech.Mount;
 import scimech.people.Pilot;
 import scimech.units.fixtures.APCannon;
+import scimech.units.fixtures.AcidFoam;
 import scimech.units.fixtures.ArcThrower;
+import scimech.units.fixtures.CorrosiveDrill;
 import scimech.units.fixtures.HeatVent;
 import scimech.units.fixtures.HotLaser;
 import scimech.units.fixtures.Krakatoa;
@@ -16,6 +18,7 @@ import scimech.units.fixtures.ZeusRifle;
 import scimech.units.mounts.Blunderbuss;
 import scimech.units.mounts.Broadside;
 import scimech.units.mounts.Foil;
+import scimech.units.mounts.Handcannon;
 import scimech.units.mounts.Pulsar;
 import scimech.units.systems.AblativeArmor;
 import scimech.units.systems.FrostAegis;
@@ -25,37 +28,27 @@ import scimech.units.systems.Plating;
 import trawel.extra;
 import trawel.randomLists;
 
-public class Pyro extends Mech {
+public class Hazmat extends Mech {
 	
-	public Pyro(boolean side) {
+	public Hazmat(boolean side) {
 		playerControlled = side;
 		complexityCap = 120;
-		weightCap = 70;
+		weightCap = 60;
 		
 		callsign = randomLists.randomElement();
 		pilot = new Pilot();
 	
 		Mount m = new Blunderbuss();
 		this.addMount(m);
-		m.addFixture(new Krakatoa());
-		m.addFixture(new HeatVent());
+		m.addFixture(new CorrosiveDrill());
 		
-		m = new Blunderbuss();
+		m = new Handcannon();
 		this.addMount(m);
-		m.addFixture(new Krakatoa());
-		m.addFixture(new HeatVent());
-		
-		m = new Foil();
-		this.addMount(m);
-		m.addFixture(new HotLaser());
-		m.addFixture(new HotLaser());
+		m.addFixture(new AcidFoam());
 		
 		this.addSystem(new FusionReactor());
 		this.addSystem(new FusionReactor());
 		this.addSystem(new FusionReactor());
-		this.addSystem(new FusionReactor());
-		
-		this.addSystem(new FrostAegis());
 		
 		
 		hp = this.getMaxHP();
@@ -63,32 +56,32 @@ public class Pyro extends Mech {
 
 	@Override
 	public int baseHP() {
-		return 210;
+		return 200;
 	}
 
 	@Override
 	public int baseSpeed() {
-		return 6;
+		return 8;
 	}
 
 	@Override
 	public int baseComplexity() {
-		return 10;
+		return 20;
 	}
 
 	@Override
 	public String getName() {
-		return "Pyro";
+		return "Hazmat";
 	}
 
 	@Override
 	public int baseDodge() {
-		return 6;
+		return 4;
 	}
 
 	@Override
 	public int baseHeatCap() {
-		return 40;
+		return 20;
 	}
 
 	@Override
@@ -99,7 +92,11 @@ public class Pyro extends Mech {
 		map.put(DamageMods.NORMAL,1f,1f);
 		map.put(DamageMods.HOLLOW,1.5f, 1f);
 		
-		map.put(DamageTypes.BURN,0.6f,0.7f,0.7f);
+		map.put(DamageTypes.CAUSTIC,0.6f,0.7f,0.5f);
+		map.put(DamageTypes.BURN,0.9f,0.9f,0.5f);
+		map.put(DamageTypes.KINETIC,1f,1f,0.5f);
+		map.put(DamageTypes.BLAST,0.95f,1f,0.5f);
+		map.put(DamageTypes.SHOCK,0.8f,0.8f,0.5f);
 		return map;
 	}
 
