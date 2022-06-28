e5f222986d7ee6723243646f83169741b2a07669
dragon <43188351+realDragon11@users.noreply.github.com>
2022-06-28 07:33:50 -0400

    Update SinE.java

--- Dragon's Trawel/src/sin/SinE.java
+++ Dragon's Trawel/src/sin/SinE.java
@@ -8,10 +8,17 @@ public class SinE {
 	
 	
 	private HashMap<String,SinRelation> relations = new HashMap<String,SinRelation>();
+	private HashMap<String,Integer> flags = new HashMap<String,Integer>();
 	
-	
+	public void setGUID(int guid) {
+		this.guid = guid;
+	}
 	public int getGUID() {
 		return guid;
+	}
+	
+	public SinRelation getRelations(int guid) {
+		return relations.get(""+guid);
 	}
 	
 	
@@ -26,6 +33,13 @@ public class SinE {
 		String[] srArr = (String[]) relations.keySet().toArray();
 		for(String sr: srArr) {
 			str+=">"+sr + relations.get(sr).saveString();
+		}
+		str+="|";
+		//flags section
+		str+="|";
+		String[] flagArr = (String[]) flags.keySet().toArray();
+		for(String fr: flagArr) {
+			str+=fr + relations.get(fr)+",";
 		}
 		str+="|";
 		return str;
