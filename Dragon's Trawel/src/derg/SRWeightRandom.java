b421866ba24aef7655cdda8bbbaad5905fd6f85a
dragon <43188351+realDragon11@users.noreply.github.com>
2023-07-16 16:03:15 -0400

    Update SRWeightRandom.java
    streamapi, why

--- Dragon's Trawel/src/derg/SRWeightRandom.java
+++ Dragon's Trawel/src/derg/SRWeightRandom.java
@@ -1,10 +1,12 @@
 package derg;
 
 import java.util.Arrays;
-import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import com.github.tommyettinger.random.EnhancedRandom;
 import com.github.tommyettinger.random.WhiskerRandom;
@@ -12,11 +14,18 @@ import com.github.tommyettinger.random.WhiskerRandom;
 public class SRWeightRandom extends StringResult {
 
 	private List<StringFloatWeight> contents;
+	private Predicate<Float[]> tolerance;
+	
+	@FunctionalInterface
+	interface ToleranceFunction<Return, a, b> {
+	    public Return apply(a one, b two);
+	}
 	
 	private static EnhancedRandom random = new WhiskerRandom(); 
 	
-	public SRWeightRandom(List<StringFloatWeight> list) {
+	public SRWeightRandom(List<StringFloatWeight> list,Predicate<Float[]> tolerance) {
 		contents = list;
+		this.tolerance = tolerance;
 	}
 
 	@Override
@@ -35,14 +44,20 @@ public class SRWeightRandom extends StringResult {
 		return list.get((int)(Math.random()*list.size()));
 	}
 	
-	@SuppressWarnings("unchecked")
 	@Override
 	public List<String> backing(){
 		return contents.stream().flatMap(a -> Arrays.asList(new String[] {a.result}).stream()).collect(Collectors.toList());
 	}
 	
+	private static final ToleranceFunction<Float[],Float,Float> flattener = (Float a, Float b) -> {return new Float[] {a,b};};
+	
 	@Override
 	public String with(StringContext context) {
+		//context.floatMap.getOrDefault(fw.weights, -1f)
+		contents.stream().filter(a -> a.weights.stream().flatMap(flattener).allMatch(tolerance));
+		//.filter((a) -> ((context.floatMap.getOrDefault(fw.weights, -1f))));
+		//context.floatMap.getOrDefault(s.weights, -1f)
+		//allMatch
 		return any();
 	}
 	
@@ -50,12 +65,41 @@ public class SRWeightRandom extends StringResult {
 	//could do sync'd lists but that'd be annoying for replication
 	public class StringFloatWeight{
 		public final String result;
-		public final float[] floats;
-		public final String[] strings;
-		public StringFloatWeight(String result, float[] floats, String[] strings) {
+		//public final float[] floats;
+		//public final String[] strings;
+		public final List<FloatWeight> weights;
+		//public final Map<String,Float> map = new HashMap<String,Float>;
+		/**
+		 * 
+		 * @param result
+		 * @param floats, use -1 to indicate should not have, 0 to indicate is fine but doesn't have [-2 is used internally for normal doesn't have]
+		 * @param strings
+		 */
+		public StringFloatWeight(String result, FloatWeight[] weights) {
 			this.result = result;
-			this.floats = floats;
-			this.strings = strings;
+			/*
+			for (int i = floats.length-1;i>=0;i--) {
+				map.put(strings[i], floats[i]);
+			}*/
+			//this.floats = floats;
+			//this.strings = strings;
+			this.weights = Arrays.asList(weights);
+		}
+		
+		/*
+		//unsure if hashmap would be better given overhead
+		public float getWeight(String name) {
+			return map.getOrDefault(name,-2f);
+		}
+		*/
+	}
+	public class FloatWeight{
+		public final float f;
+		public final String s;
+		
+		public FloatWeight(String str, float floa) {
+			s = str;
+			f = floa;
 		}
 	}
 	
