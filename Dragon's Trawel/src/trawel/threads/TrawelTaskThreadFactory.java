28313b91eaef7d3511892f50aa8c6777b016afb5
dragon <43188351+realDragon11@users.noreply.github.com>
2023-07-18 02:03:17 -0400

    seems to start to work, weird it didn't say other errors

--- Dragon's Trawel/src/trawel/threads/TrawelTaskThreadFactory.java
+++ Dragon's Trawel/src/trawel/threads/TrawelTaskThreadFactory.java
@@ -5,13 +5,14 @@ import java.util.concurrent.ThreadFactory;
 
 public class TrawelTaskThreadFactory implements ThreadFactory{
 
-	private ThreadGroup group = new ThreadGroup("trawelTask");
+	//private ThreadGroup group = new ThreadGroup("trawelTask");
 	private int threadCounter = 0;
 	
 	@Override
 	public Thread newThread(Runnable run) {
-		Thread t = new Thread(group,null,group.getName()+"_"+ ++threadCounter);
-		//Thread t = Executors.defaultThreadFactory().newThread(run);
+		//Thread t = new PooledThread(group,null,group.getName()+"_"+ ++threadCounter);
+		Thread t = Executors.defaultThreadFactory().newThread(run);
+		t.setName("trawelTask"+"_"+ ++threadCounter);
         t.setDaemon(true);
         //t.setPriority(Thread.NORM_PRIORITY-1);
 		return t;
