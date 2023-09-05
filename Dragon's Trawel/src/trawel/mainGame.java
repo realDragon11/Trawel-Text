4b2397a46d48b74ad474409235000d40079bf970
realDragon <realDragon@mail.com>
2023-09-05 11:56:25 -0400

    spet is not a month

--- Dragon's Trawel/src/trawel/mainGame.java
+++ Dragon's Trawel/src/trawel/mainGame.java
@@ -71,7 +71,7 @@ public class mainGame {
 
 	//b__X is in development, b_X is the actual release of that version
 	public static final String VERSION_STRING = "v0.8.b_5";
-	public static final String VERSION_DATE = " updated Spet 5th 2023";
+	public static final String VERSION_DATE = " updated Sept 5th 2023";
 	public static final String[] changelog = new String[] {
 			//add to front, changeviewer cycles to older ones when used
 			"b_5: {part 1/4} You can now recruit adventurers in certain dungeons. All other dungeons had their mass battles removed. In 'dangerous' dungeons, these adventurers will assist you in the 'high security' checkpoints, and against the boss. Currently this is just the Tower of Fate and the Fatespinner. They will get a share of the loot, and if higher level than you, might get first pick. They will not be interested in joining you if they are 2 or more levels higher. They will not revive on death, and will leave if you do not visit the dungeon for a while. Fighting with them will make them take longer to leave than just visiting will.",
