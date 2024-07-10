7c39aa88c0c8e275dcc698c346681401ca2e65f3
realDragon <realDragon@mail.com>
2024-07-09 21:45:35 -0400

    give wanderendless failsafe code to not crash

--- Dragon's Trawel/src/trawel/personal/people/behaviors/WanderEndless.java
+++ Dragon's Trawel/src/trawel/personal/people/behaviors/WanderEndless.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import trawel.core.Rand;
+import trawel.core.mainGame;
 import trawel.personal.people.Agent;
 import trawel.personal.people.Behavior;
 import trawel.time.TimeEvent;
@@ -38,6 +39,12 @@ public class WanderEndless extends Behavior{
 	
 	public static Connection destination(Agent user) {
 		Town current = user.getLocation();
+		if (current == null) {
+			if (mainGame.debug) {
+				System.out.println("invalid location for wander endless on "+user.getPerson().getName());
+			}
+			return null;
+		}
 		if (current.hasConnectFlow() && Rand.chanceIn(1, 5)) {//1/5th chance to follow natural flow
 			return current.getConnectFlow();
 		}
@@ -64,7 +71,8 @@ public class WanderEndless extends Behavior{
 				return c;
 			}
 		}
-		throw new RuntimeException("ran of of weight for random wander " + user.getPerson().getName() + " in " + current.getName());
+		//return last element to protect from rounding errors
+		return connects.getLast();
 	}
 
 }
